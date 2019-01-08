5363abfd8126c1ebdd967219639b01686022a733
Heechul Ryu <ryuhcii@gmail.com>
2019-01-08 10:38:24 +0900

    Fix wrong order to require mustache

--- functions/set-env-web/handler.js
+++ functions/set-env-web/handler.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const fs = require('fs');
+const { render } = require('mustache')
+
 const config = {
   host: process.env.SET_ENV_ENDPOINT,
   orgName: process.env.TRAVIS_ORG_NAME,
@@ -10,8 +12,6 @@ const config = {
 const filename = `${config.mustachePath}/handler.mustache`
 const toBeRendered = fs.readFileSync(filename, 'utf8')
 const rendered = render(toBeRendered, config)
-
-const { render } = require('mustache')
 
 async function html(event, context) {
   const { data: body, extensions: { response: res } } = event
