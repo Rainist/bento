345177cbe295faca5a138654e7557bfdfee46c80
Heechul Ryu <ryuhcii@gmail.com>
2019-07-09 10:15:28 +0900

    Apply fmt to handler.js

--- functions/set-env/handler.js
+++ functions/set-env/handler.js
@@ -1,94 +1,94 @@
-const _ = require('lodash')
-const { from, of } = require( 'rxjs');
-const { concatMap, delay, catchError, scan, toArray } = require( 'rxjs/operators');
+const _ = require("lodash");
+const { from, of } = require("rxjs");
+const { concatMap, catchError, toArray } = require("rxjs/operators");
 
-const { envs, slackWebhookURL } = require('./lib/config')
-const { setEnv } = require('./lib/travis')
+const { envs, slackWebhookURL } = require("./lib/config");
+const { setEnv } = require("./lib/travis");
 
-const { webhook } = require('./lib/slack')
-const { notify } = webhook(slackWebhookURL)
+const { webhook } = require("./lib/slack");
+const { notify } = webhook(slackWebhookURL);
 
 function genResponse(result) {
   const allSuccess = _.chain(result)
     .map(({ success }) => success)
     .reduce((acc, curr) => acc && curr)
-    .value()
+    .value();
 
   const env = _.chain(result)
     .map(({ success, name, message }) => ({ success, name, message }))
-    .value()
+    .value();
 
-  const status = allSuccess ? 201 : 200
+  const status = allSuccess ? 201 : 200;
 
-  return { status, env, allSuccess }
+  return { status, env, allSuccess };
 }
 
 function emitResponse(res, result) {
   if (!res) {
-    console.error('no "res" provided trying to respond')
-    console.log('with the result: ', result)
-    return
+    console.error('no "res" provided trying to respond');
+    console.log("with the result: ", result);
+    return;
   }
 
-  const { status, env, allSuccess } = genResponse(result)
-  res.status(status).json({ env, allSuccess })
+  const { status, env, allSuccess } = genResponse(result);
+  res.status(status).json({ env, allSuccess });
 
-  return Promise.resolve(allSuccess)
+  return Promise.resolve(allSuccess);
 }
 
 function notifySuccess(repo, who) {
-  const text = `env has set for \`${repo}\` in travis by ${who}`
-  return notify({text})
+  const text = `env has set for \`${repo}\` in travis by ${who}`;
+  return notify({ text });
 }
 
 function notifyFail(repo, who) {
-  let text = `set env has *failed* for \`${repo}\` in travis by ${who}`
+  let text = `set env has *failed* for \`${repo}\` in travis by ${who}`;
 
-  if(!repo || !who) {
-    text = 'set env targeting travis has *failed* and either *repo* or *who* is not provided.'
+  if (!repo || !who) {
+    text =
+      "set env targeting travis has *failed* and either *repo* or *who* is not provided.";
   }
 
-  return notify({text})
+  return notify({ text });
 }
 
 async function _add(rawRepo, who, res) {
-  const repo = rawRepo.replace('/', '%2F')
+  const repo = rawRepo.replace("/", "%2F");
 
   const result = await from(envs())
     .pipe(
-      concatMap(env => 
-        from(setEnv(repo, env, env.rewrite))
-          .pipe(
-            catchError(err => of(err))
-          )
+      concatMap(env =>
+        from(setEnv(repo, env, env.rewrite)).pipe(catchError(err => of(err)))
       ),
       toArray()
     )
-    .toPromise()
+    .toPromise();
 
-  const isSuccess = await emitResponse(res, result)
+  const isSuccess = await emitResponse(res, result);
 
-  if(slackWebhookURL) {
-    const notifyFn = isSuccess ? notifySuccess : notifyFail
-    await notifyFn(rawRepo, who).catch(console.error)
+  if (slackWebhookURL) {
+    const notifyFn = isSuccess ? notifySuccess : notifyFail;
+    await notifyFn(rawRepo, who).catch(console.error);
   }
 }
 
 async function add(event, context) {
-  const { data: body, extensions: { response: res } } = event
-  const { repo, who } = body
+  const {
+    data: body,
+    extensions: { response: res }
+  } = event;
+  const { repo, who } = body;
 
   try {
-    await _add(repo, who, res)
-  }
-  catch (e) {
-    console.error(e)
+    await _add(repo, who, res);
+  } catch (e) {
+    console.error(e);
     if (res) {
-      res.status(500).end('something went wrong')
+      res.status(500).end("something went wrong");
     }
 
-    notifyFail(repo, who).catch(console.error)
+    notifyFail(repo, who).catch(console.error);
   }
 }
 
-module.exports = { add }
+module.exports = { add };
